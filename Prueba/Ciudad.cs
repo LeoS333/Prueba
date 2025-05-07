c70b5ebcb6938c957e6a236fbb6627511d2cf3dd
info-1 <info-1@INFO1-PC8>
2025-05-07 19:06:19 -0300

    Agregar archivos de proyecto.

---
+++ Prueba/Ciudad.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba
+{
+    class Ciudad
+    {
+        string Nombre { get; set; }
+
+        int CantHabitantes { get; set; }
+        int SuperficieKM { get; set; }
+    }
+}
---
+++ Prueba/Provincia.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba
+{
+    class Provincia
+    {
+        string Nombre { get; set; }
+        string Gobernador{ get; set; }
+        string Region { get; set; }
+        string ListaCiudades { get; set; }
+
+
+    }
+}
